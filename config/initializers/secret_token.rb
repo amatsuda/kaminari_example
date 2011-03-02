007d56238d0b20520421352d5465dadcb323c073
Akira Matsuda <ronnie@dio.jp>
2011-03-02 11:36:56 +0900

    rails new kaminari_example

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KaminariExample::Application.config.secret_token = 'e6bf1fdc416600a426725c0ade254049604474b33f52231ed17d520abbd7e6e190432b560b4a5bbbdd056b69d733611c300c66c7a202cb2d54c452503b0f18e3'
