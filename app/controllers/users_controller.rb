1fed168ba450554d655332d0c8c04ffc3f3c3b02
Akira Matsuda <ronnie@dio.jp>
2011-03-02 11:42:54 +0900

    implement pagination
    * pass :page parameter to User.page
    * add paginate helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.xml
   def index
-    @users = User.all
+    @users = User.page(params[:page])
 
     respond_to do |format|
       format.html # index.html.erb
